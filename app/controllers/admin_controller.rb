fd388b6e5ad3bcf41abc4378452d5b1ed380b281
kyoya anjo <>
2019-02-10 12:34:38 +0900

    従業員の勤怠を一覧で表示できるように

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,22 +1,29 @@
 class AdminController < ApplicationController
   before_action :set_user
+  before_action :set_employee,only: [:show,:attendance,:earning]
   before_action :authenticate_user!
   before_action :authenticate_admin!
   def index
+    @employees=User.all
   end
 
   def show
   end
 
   def attendance
+    @attendances=Attendance.where(user_id:@employee.id)
   end
 
   def earning
+    @earnings=Earning.all
   end
 
   private
   def set_user
     @user = User.find(current_user.id)
+  end
+  def set_employee
+    @employee = User.find(params[:id])
   end
 
   def authenticate_admin!
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def working_hour(in_time,out_time)
+    sec=out_time-in_time
+    (Time.parse("1/1") + (sec)).strftime("%H時間%M分%S秒")
+  end
 end
