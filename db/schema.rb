bb83d64116d6649ad73da40c328c16b62dbc7cb4
kyoya anjo <>
2019-02-16 10:35:13 +0900

    現在地情報の取得（未完成）

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_10_050449) do
+ActiveRecord::Schema.define(version: 2019_02_12_100930) do
 
   create_table "attendances", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -27,6 +27,9 @@ ActiveRecord::Schema.define(version: 2019_02_10_050449) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "location", default: "", null: false
+    t.string "address", null: false
+    t.float "longitude", null: false
+    t.float "latitude", null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
